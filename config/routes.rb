c0b9d1614997098f6e0e922318b8676dd65b2894
kamura5656 <78400968+kamura5656@users.noreply.github.com>
2021-03-01 12:47:49 +0900

    エラーコード基本部分記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'items/index'
+  devise_for :users
+  root to: 'items#index'
+  resources :items, only: [:index]
+  resources :users, only: [:show]
 end
