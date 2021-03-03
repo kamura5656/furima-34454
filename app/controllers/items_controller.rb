5ecb397592819ad3dcf4ae271a43f9147e588648
kamura5656 <78400968+kamura5656@users.noreply.github.com>
2021-03-03 21:21:45 +0900

    レビューにて指摘をいただいた箇所を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index]
+  before_action :authenticate_user!, only: [:new ]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -25,9 +25,5 @@ class ItemsController < ApplicationController
       :name, :contents, :category_id, :status_id, :delivery_fee_id,
       :delivery_day_id, :first_address_id, :price, :image
     ).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
   end
 end
