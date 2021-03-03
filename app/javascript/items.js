7b3624d64c0b2972cbb046258101955eadaf996e
kamura5656 <78400968+kamura5656@users.noreply.github.com>
2021-03-03 12:41:31 +0900

    JSによる金額計算部分を実装

--- app/javascript/items.js
+++ app/javascript/items.js
@@ -1,12 +1,14 @@
-window.addEventListener('Load', function(){
+window.addEventListener('load', function(){
   const itemPrice = document.getElementById("item-price");
   const addTaxPrice = document.getElementById("add-tax-price");
   const profit = document.getElementById("profit");
 
   itemPrice.addEventListener('input', function(){
     // item-priceの金額（値）を取り、変数Aに代入。
-  const formPrice = document.getElementById("item-price");
+  const formPrice = itemPrice.value;
     // innerHTMLで差し込みたいHTMLにMath.floorを使って計算（変数Aを使う）。
+    addTaxPrice.innerHTML = Math.floor(formPrice/10);
     // innerHTMLで差し込みたいHTMLにMath.floorを使って計算（変数Aを使う）。
-  })
+    profit.innerHTML = Math.floor((formPrice/10)*9);
+  });
 });
