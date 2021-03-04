2109869c2b188e9150d878e7691422720f37761a
kamura5656 <78400968+kamura5656@users.noreply.github.com>
2021-03-04 17:52:09 +0900

    URL入力等による他人の編集ページへの遷移を禁止

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :params_find,        only: [:edit, :update, :show]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index,      only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,7 +21,18 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
@@ -29,5 +42,15 @@ class ItemsController < ApplicationController
       :name, :contents, :category_id, :status_id, :delivery_fee_id,
       :delivery_day_id, :first_address_id, :price, :image
     ).merge(user_id: current_user.id)
+  end
+
+  def params_find
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless  current_user.id == @item.user_id 
+      redirect_to action: :index
+    end
   end
 end
