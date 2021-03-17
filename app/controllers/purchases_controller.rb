b964a5d00fe56240fdf6672686e7afc97ddee1d9
kamura5656 <78400968+kamura5656@users.noreply.github.com>
2021-03-17 15:13:56 +0900

    購入済み商品の購入ページに移動できる不具合を修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -28,7 +28,7 @@ class PurchasesController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if current_user.id == @item.user_id
+    redirect_to root_path if current_user.id == @item.user_id || Purchase.pluck(:item_id).include?(@item.id)
   end
 
   def params_find
